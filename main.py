c9e95d8e9ea20789b58eb6bdbae0c4c5416a35d8
rmansurbek2197 <rmansurbek2197@gmail.com>
2026-04-15 23:42:54 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# 61
+nums=[1,2,3]
+print([x+10 for x in nums])
+
+# 62
+for i in range(10):
+    if i%2:
+        print(i)
+
+# 63
+text="python"
+print(len(text))
+
+# 64
+nums=[1,2,3]
+print(sum(nums))
+
+# 65
+for i in range(3):
+    for j in range(3):
+        print(i,j)
+
+# 66
+print("abc".upper())
+
+# 67
+print("ABC".lower())
+
+# 68
+print(list("hello"))
+
+# 69
+nums=[2,4,6]
+print(max(nums))
+
+# 70
+nums=[2,4,6]
+print(min(nums))
